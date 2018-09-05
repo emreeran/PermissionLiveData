2b969712ebe265b7938b53a1ec7e3963f11c326b
Emre Eran <emre.eran@gmail.com>
2018-09-05 21:42:23 +0300

    Added sample usage to MainActivity

--- sample/src/main/java/com/emreeran/permissionlivedata/MainActivity.kt
+++ sample/src/main/java/com/emreeran/permissionlivedata/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.emreeran.permissionlivedata
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        Timber.plant(Timber.DebugTree())
+
+        val data = PermissionLiveData(this).request(
+                Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        data.observe(this, Observer {
+            Timber.d("Permission received: $it")
+        })
     }
 }
